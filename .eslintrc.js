dbb3e2938e129dcc00c6c1a8af1993b7d38e2e3e
Tai_SaWFly <vawer24@gmail.com>
2022-07-31 12:06:40 +0500

    pagination

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,26 +1,30 @@
 module.exports = {
-  env: {
-    browser: true,
-    es2021: true,
-  },
-  extends: ["plugin:react/recommended", "standard"],
-  parserOptions: {
-    ecmaFeatures: {
-      jsx: true,
+    env: {
+        browser: true,
+        es2021: true
     },
-    ecmaVersion: "latest",
-    sourceType: "module",
-  },
-  plugins: ["react"],
-  rules: {
-    indent: [0, 4],
-    semi: [2, "always"],
-    "space-before-function-paren": ["error", "never"],
-    quotes: ["error", "double", { allowTemplateLiterals: true }],
-
-    "max-len": ["error", { code: 100 }],
-    // "comma-dangle": [0, "always"],
-    // "comma-dangle": [2, "always-multiline"],
-    "multiline-ternary": "off",
-  },
+    extends: ["plugin:react/recommended", "standard"],
+    parserOptions: {
+        ecmaFeatures: {
+            jsx: true
+        },
+        ecmaVersion: 12,
+        sourceType: "module"
+    },
+    plugins: ["react"],
+    rules: {
+        semi: [2, "always"],
+        indent: ["error", 4],
+        "space-before-function-paren": [
+            "error",
+            { anonymous: "always", named: "never" }
+        ],
+        quotes: [
+            "error",
+            "double",
+            {
+                allowTemplateLiterals: true
+            }
+        ]
+    }
 };
--- .prettirerc.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-  printWidth: 100,
-  trailingComma: "es5",
-  tabWidth: 4,
-  semi: true,
-};
